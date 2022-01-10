fd8e396535683781c1ccc022f123f563dc8d44cc
codefe <849322417@qq.com>
2022-01-10 13:35:56 +0800

    add jest

--- app/data/db.js
+++ app/data/db.js
@@ -4439,6 +4439,8 @@ let db = [
                     {
                         id: 7722034,
                         title: 'Jest测试',
+                        flag: 'recommend',
+                        icon: '/app/img/edu/jest.jpg',
                         desc: 'Jest是 Facebook 的一套开源的 JavaScript 测试框架, 它自动集成了断言、JSDom、覆盖率报告等开发者所需要的所有测试工具,是一款几乎零配置的测试框架。',
                         child: [
                             {
